ec805646d01085da5648e19f374997186b08762b
Yongzhe Zhang <jcddyongzhe@gmail.com>
2020-01-04 01:10:21 +0800

    LL to construct the Dictionary node
    LL's performance is not good as BST

--- src/spelling/DictionaryLL.java
+++ src/spelling/DictionaryLL.java
@@ -8,10 +8,11 @@ import java.util.LinkedList;
  */
 public class DictionaryLL implements Dictionary 
 {
-
-	private LinkedList<String> dict;
-	
+	private LinkedList<String> dict;	
     // TODO: Add a constructor
+	public DictionaryLL(){
+		dict = new LinkedList<String>();
+	}
 
 
     /** Add this word to the dictionary.  Convert it to lowercase first
@@ -21,7 +22,15 @@ public class DictionaryLL implements Dictionary
      * (it wasn't already there). */
     public boolean addWord(String word) {
     	// TODO: Implement this method
+    	if(word == null) {
         return false;
+    	}
+    	word = word.toLowerCase();
+    	if(dict != null && dict.contains(word)) {
+    		return false;
+    	}
+    	dict.add(word);
+    	return true;
     }
 
 
@@ -29,14 +38,19 @@ public class DictionaryLL implements Dictionary
     public int size()
     {
         // TODO: Implement this method
-        return 0;
+        return dict.size();
     }
 
     /** Is this a word according to this dictionary? */
     public boolean isWord(String s) {
         //TODO: Implement this method
+    	if(dict == null || s == null) {
+    		return false;
+    	}
+    	s = s.toLowerCase();
+    	if(dict.contains(s)) {
+    			return true;
+    	}
         return false;
-    }
-
-    
+    }  
 }
